47929de4535e48710d77bdbbf3c34cec81ab37ff
BKinya <gitongakinya1@gmail.com>
2023-08-23 12:38:38 +0300

    update starter project

--- 03-create-room-entities/starter/app/src/main/java/com/kodeco/android/librarian/di/AppModules.kt
+++ 03-create-room-entities/starter/app/src/main/java/com/kodeco/android/librarian/di/AppModules.kt
@@ -41,7 +41,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val databaseModule = module {
-
+  // TODO: Create a database instance
 }
 
 val repositoryModule = module {
